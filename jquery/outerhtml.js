eed36dacea7e7102e877ff3ec271a5c43ca426ab
shaan1974 <stephane.liuzzi@perso.be>
2020-09-06 13:48:00 +0200

    1.3a

--- jquery/outerhtml.js
+++ jquery/outerhtml.js
@@ -1,14 +1,14 @@
-		$.fn.outerHTML = function() 
+		$.fn.outerHTML = function()
 		{
-				var $t = $(this);				
-				if ($t[0].outerHTML !== undefined) 
-				{
-					return $t[0].outerHTML;
-				} 
-				else 
-				{
-					var content = $t.wrap('<div/>').parent().html();
-					$t.unwrap();
-					return content;
-				}
-		};	
+		    var $t = $(this);
+		    if ($t[0].outerHTML !== undefined)
+		    {
+		        return $t[0].outerHTML;
+		    }
+		    else
+		    {
+		        var content = $t.wrap('<div/>').parent().html();
+		        $t.unwrap();
+		        return content;
+		    }
+		};
